ec0e2a8993935627856efa3966a180984e7d6c65
Massimiliano <massimiliano@Wolf-pippo.local>
2018-10-16 15:31:23 +0200

    solo per update version

--- Example/CommonUI/ViewController.swift
+++ Example/CommonUI/ViewController.swift
@@ -11,6 +11,9 @@ import CommonUI
 
 class ViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     @IBAction func showAlert() {
         AlertViewController.show(withTitle: "Example", message: "This is a test message", buttonTitles: ["Option one", "Option two"], cancelButton: nil) { (sender) in
             
